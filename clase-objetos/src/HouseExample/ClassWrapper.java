a98060d01f7fc8c4f5a379f77580d142322e9a8e
CesarLinux <eduardomelchor34@gmail.com>
2025-10-25 21:16:29 -0600

    values as cclasses and not primitives

--- clase-objetos/src/HouseExample/ClassWrapper.java
+++ clase-objetos/src/HouseExample/ClassWrapper.java
@@ -3,6 +3,8 @@ package HouseExample;
 public class ClassWrapper {
     public static void main(String[] args) {
         int number = 10;
-        Integer number2 = 10;
+        Integer number2 = Integer.valueOf(3 );
+
+        System.out.println(number2);
     }
 }
