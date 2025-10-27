40db9f58fa6137021dd6fb112f1792ec863951ea
CesarLinux <eduardomelchor34@gmail.com>
2025-10-26 22:53:35 -0600

    Proyecto final - parte 2

--- clase-objetos/src/ProyectoFinal/BankApp.java
+++ clase-objetos/src/ProyectoFinal/BankApp.java
@@ -10,4 +10,61 @@ public class BankApp {
         this.account = account;
         this.scanner = new Scanner(System.in);
     }
+
+    public void start(){
+        int option;
+        do {
+            displayMenu();
+            option = scanner.nextInt();
+            processOption(option);
+        }while (option != 4);
+    }
+
+    private void displayMenu(){
+        System.out.println("\nSeleccciona opciones");
+        System.out.println("1 - Depositar dinero");
+        System.out.println("2 - Retirar dinero");
+        System.out.println("3 - Mostrar saldo");
+        System.out.println("4 - Salir");
+    }
+
+    private int getUserOption(){
+        return scanner.nextInt();
+    }
+
+    private void handleDeposit(){
+        System.out.println("Ingrese el monto a depositar");
+        double depositAmount = scanner.nextDouble();
+        account.deposit(depositAmount);
+    }
+    void handleWithdrawal(){
+        System.out.println("Ingrese el monto a retirar");
+        double withdrawAmount = scanner.nextDouble();
+        account.withdraw(withdrawAmount);
+    }
+
+    private void processOption(int option){
+        switch (option){
+            case 1:{
+                handleDeposit();
+                break;
+            }
+            case 2:{
+                handleWithdrawal();
+                break;
+            }
+
+            case 3:{
+                account.displayBalance();
+                break;
+            }
+            case 4:{
+                System.out.println("Gracias por utilizar la app");
+                break;
+            }
+            default:{
+                System.out.println("Opcion incorrecta");
+            }
+        }
+    }
 }
