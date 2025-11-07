509cc0f3931b2d36dd0a5181d5f27fd46185655b
CesarLinux <eduardomelchor34@gmail.com>
2025-11-06 23:31:38 -0600

    Entendiendo el metodo: equals

---
+++ 06-arrays-y-colecciones/src/com/devtalles/esctructuradedatos/equals/ExampleEquals.java
@@ -0,0 +1,11 @@
+package com.devtalles.esctructuradedatos.equals;
+
+public class ExampleEquals {
+    public static void main(String[] args) {
+        Integer numberA = 127;
+        Integer numberB = 127;
+
+        System.out.println(numberB == numberA);
+        System.out.println(numberB.equals(numberA));
+    }
+}
