6af617922692c6f17e423dc6fa6dd5cf0618970f
CesarLinux <eduardomelchor34@gmail.com>
2025-11-06 18:06:34 -0600

    Arrays parte 2

--- 06-arrays-y-colecciones/src/Main.java
+++ 06-arrays-y-colecciones/src/Main.java
@@ -4,9 +4,23 @@ import java.util.ArrayList;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] numbersPrimitive = new int[5];
+        int[] numbersPrimitive = {1,2,3,4,5,6,0,9};
 
-        numbersPrimitive[0] = 10;
+        int deletePosition = 2;
+
+        for(int i = deletePosition; i < numbersPrimitive.length - 1; i++){
+            numbersPrimitive[i] = numbersPrimitive[i+1];
+        }
+
+
+        numbersPrimitive[0] = 200;
+
+        numbersPrimitive[numbersPrimitive.length-1] = 0;
+        for (int number: numbersPrimitive){
+            System.out.println(number);
+        }
+
+        /*numbersPrimitive[0] = 10;
         numbersPrimitive[1] = 20;
         int[] newNumbers = new int[numbersPrimitive.length + 1];
         System.arraycopy(numbersPrimitive, 0, newNumbers, 0, numbersPrimitive.length);
@@ -27,7 +41,7 @@ public class Main {
 
         for (Integer number: numbersWrapper){
             System.out.println(number);
-        }
+        }*/
 
     }
 }
