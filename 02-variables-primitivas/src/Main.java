648ef57bbbe3d5e0ff7edffcccfe35b75d04468a
CesarLinux <eduardomelchor34@gmail.com>
2026-03-06 08:55:33 -0600

    Variables primitivas

---
+++ 02-variables-primitivas/src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        byte age = 23;
+        System.out.println("age = " + age);
+        double price = 19.99f;
+        int xAxis, yAxis;
+        xAxis=10;
+        yAxis=20;
+        age = 30;
+        System.out.println("age = " + age);
+
+        System.out.println("xAxis = " + xAxis);
+        System.out.println("yAxis = " + yAxis);
+
+        char initial = '1';
+        System.out.println("Initial = " + initial);
+        boolean isTeacher = true;
+
+    }
+}
