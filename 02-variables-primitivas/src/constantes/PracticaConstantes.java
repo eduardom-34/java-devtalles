c1ad3b518043f2764214692c85c1f7ca9f7c881b
CesarLinux <eduardomelchor34@gmail.com>
2026-03-08 22:42:13 -0600

    Constantes - Buenas prácticas

---
+++ 02-variables-primitivas/src/constantes/PracticaConstantes.java
@@ -0,0 +1,12 @@
+package constantes;
+
+public class PracticaConstantes {
+
+    public static void main(String[] args) {
+        final double TAX_RATE = 0.10;
+        int grossSalary = 3000;
+        double netSalary = grossSalary * (1 - TAX_RATE);
+        System.out.println("netSalary = " + netSalary);
+    }
+
+}
