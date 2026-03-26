7c68fb4f48a536e09c670cd194953fc2f8f96ebb
CesarLinux <eduardomelchor34@gmail.com>
2026-03-25 21:51:04 -0600

    Bucle - for

---
+++ 02-variables-primitivas/src/bucles/Bucle.java
@@ -0,0 +1,11 @@
+package bucles;
+
+public class Bucle {
+    public static void main(String[] args) {
+        for ( int i = 2; i <= 5 ; i+=2){
+            System.out.println("Valor: " + i);
+        }
+
+        System.out.println("sale");
+    }
+}
